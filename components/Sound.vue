8e8e5cf5909b079ecc1cec7b3d339526efacbe9a
Isobella Bradshaw <isobella89@gmail.com>
2019-08-04 20:00:06 +0100

    added step component

--- components/Sound.vue
+++ components/Sound.vue
@@ -1,129 +1,78 @@
 <template>
 <div>
-  <!-- <audio src="@/assets/sounds/clap.wav"></audio> -->
-
   <v-btn v-on:click="start">Play audio</v-btn>
 
   <v-btn v-on:click="stop">Stop audio</v-btn>
+
+<ul>
+  <li v-for="(step, index) in pattern.tracks[0].steps">
+    <Step :index="index" :active="step" :playing="index === currentStep" />
+  </li>
+</ul>
+
+
 </div>
 </template>
 
 <script>
 import AudioEngine from '~/plugins/audioEngine.js'
+import Step from '~/components/Step.vue'
 
 const pattern = {
     name: "botthisway",
     stepCount: 16,
     beatsPerMinute: 100,
     tracks: [
+        // {
+        //   instrument: "hihat",
+        //   steps: [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+        // },
         {
-            instrument: "hihat",
-            steps: [
-                1,
-                0,
-                1,
-                0,
-                1,
-                0,
-                1,
-                0,
-                1,
-                0,
-                1,
-                0,
-                1,
-                0,
-                1,
-                0
-            ]
+          instrument: "snare",
+          steps: [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]
         },
-        {
-            instrument: "snare",
-            steps: [
-                0,
-                0,
-                0,
-                0,
-                1,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                1,
-                0,
-                0,
-                0
-            ]
-        },
-        {
-            instrument: "clap",
-            steps: [
-                0,
-                0,
-                0,
-                0,
-                1,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                1,
-                0,
-                0,
-                0
-            ]
-        },
-        {
-            instrument: "kick",
-            steps: [
-                1,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                1,
-                1,
-                0,
-                1,
-                0,
-                0,
-                0,
-                0,
-                0
-            ]
-        }
+        // {
+        //   instrument: "clap",
+        //   steps: [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]
+        // },
+        // {
+        //   instrument: "kick",
+        //   steps: [1,0,0,0,0,0,0,1,1,0,1,0,0,0,0,0]
+        // }
     ]
 }
 
   export default {
     name: "Sound",
+    components: {
+      Step
+    },
     created: function () {
-      const love = new AudioEngine({ onStep: ({ position }) => {
+      const audioEngine = new AudioEngine({ onStep: ({ position }) => {
         this.onStep(position)
       }})
-      love.prepare().then(() => {
-        love.setPattern(pattern)
+
+      audioEngine.prepare().then(() => {
+        audioEngine.setPattern(this.$data.pattern)
       })
 
-      this.$data.love = love
+      this.$data.audioEngine = audioEngine
+    },
+    data: () => {
+      return {
+        pattern: pattern,
+        currentStep: -1
+      }
     },
     methods: {
       start: function () {
-        this.$data.love.startClock(60);
+        this.$data.audioEngine.startClock(30);
       },
       stop: function () {
-        this.$data.love.stopClock();
+        this.$data.audioEngine.stopClock();
       },
       onStep: function (position) {
-        console.log('hello', position)
+        this.$data.currentStep = position.step
       }
     }
   }
---
+++ components/Step.vue
@@ -0,0 +1,32 @@
+<template>
+    <span :class="{ active, playing }">step {{index + 1}}</span>
+</template>
+
+<style>
+  .active {
+    color: red;
+  }
+  .playing {
+    border: 1px solid red;
+  }
+</style>
+
+<script>
+  export default {
+    name: "Step",
+    props: {
+      index: {
+        type: Number,
+        required: true
+      },
+      active: {
+        type: Number,
+        required: true
+      },
+      playing: {
+        type: Boolean,
+        required: true
+      }
+    }
+  }
+</script>
