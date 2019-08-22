747bffa47ea48e80ee446a87478bf90970b93da4
Isobella Bradshaw <isobella89@gmail.com>
2019-08-22 16:50:57 +0100

    patterns store module

---
+++ store/patterns.js
@@ -0,0 +1,229 @@
+export const state = () => ({
+  patterns: [
+    {
+      'name': 'oontza',
+      'stepCount': 16,
+      'beatsPerMinute': 110,
+      'tracks': [
+        {
+          'instrument': 'hihat',
+          'steps': [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1]
+        },
+        {
+          'instrument': 'snare',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+        }
+      ]
+    },
+    {
+      'name': 'bossanoopa',
+      'stepCount': 16,
+      'beatsPerMinute': 110,
+      'tracks': [
+        {
+          'instrument': 'ride',
+          'steps': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+        },
+        {
+          'instrument': 'hihat',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'rim',
+          'steps': [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1]
+        }
+      ]
+    },
+    {
+      'name': 'nipnop',
+      'stepCount': 16,
+      'beatsPerMinute': 92,
+      'tracks': [
+        {
+          'instrument': 'snare',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'clap',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+        },
+        {
+          'instrument': 'cowbell',
+          'steps': [0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+        }
+      ]
+    },
+    {
+      'name': 'botthisway',
+      'stepCount': 16,
+      'beatsPerMinute': 100,
+      'tracks': [
+        {
+          'instrument': 'hihat',
+          'steps': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+        },
+        {
+          'instrument': 'snare',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'clap',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0]
+        }
+      ]
+    },
+    {
+      'name': 'funkee',
+      'stepCount': 16,
+      'beatsPerMinute': 94,
+      'tracks': [
+        {
+          'instrument': 'hihat',
+          'steps': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        },
+        {
+          'instrument': 'rim',
+          'steps': [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0]
+        },
+        {
+          'instrument': 'snare',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0]
+        }
+      ]
+    },
+    {
+      'name': 'shlojam',
+      'stepCount': 16,
+      'beatsPerMinute': 72,
+      'tracks': [
+        {
+          'instrument': 'hihat',
+          'steps': [1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1]
+        },
+        {
+          'instrument': 'rim',
+          'steps': [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0]
+        },
+        {
+          'instrument': 'clap',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+        }
+      ]
+    },
+    {
+      'name': 'botorik',
+      'stepCount': 16,
+      'beatsPerMinute': 120,
+      'tracks': [
+        {
+          'instrument': 'hihat',
+          'steps': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+        },
+        {
+          'instrument': 'snare',
+          'steps': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0]
+        },
+        {
+          'instrument': 'cowbell',
+          'steps': [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+        }
+      ]
+    },
+    {
+      'name': 'swoop',
+      'stepCount': 12,
+      'beatsPerMinute': 95,
+      'tracks': [
+        {
+          'instrument': 'ride',
+          'steps': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+        },
+        {
+          'instrument': 'hihat',
+          'steps': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0]
+        },
+        {
+          'instrument': 'rim',
+          'steps': [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0]
+        }
+      ]
+    },
+    {
+      'name': 'schmaltz',
+      'stepCount': 12,
+      'beatsPerMinute': 105,
+      'tracks': [
+        {
+          'instrument': 'ride',
+          'steps': [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0]
+        },
+        {
+          'instrument': 'hihat',
+          'steps': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0]
+        },
+        {
+          'instrument': 'rim',
+          'steps': [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+        }
+      ]
+    },
+    {
+      'name': 'bouncy',
+      'stepCount': 16,
+      'beatsPerMinute': 125,
+      'tracks': [
+        {
+          'instrument': 'hihat',
+          'steps': [1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1]
+        },
+        {
+          'instrument': 'rim',
+          'steps': [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0]
+        },
+        {
+          'instrument': 'kick',
+          'steps': [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+        }
+      ]
+    }
+  ]
+})
+
+export const mutations = {
+}
